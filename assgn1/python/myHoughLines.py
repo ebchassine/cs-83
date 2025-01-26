542251c6c9567ffbe47e47d65c07c1d4dd1cb18f
ebchassine <ebchassine@gmail.com>
2025-01-26 14:33:01 -0500

    Removed Normalization step from myHoughLines

--- assgn1/python/myHoughLines.py
+++ assgn1/python/myHoughLines.py
@@ -1,20 +1,18 @@
 import numpy as np
-import cv2  # For cv2.dilate function
+# import cv2  # For cv2.dilate function
 
 # img_hough (np.ndarray): The Hough transform accumulator (2D array).
-# nLines (int): The number of lines to detect.
+# nLines (int): The number of lines to 
 def myHoughLines(img_hough, nLines):
-    # Normalize the accumulator
-    normalized_hough = img_hough / np.max(img_hough)
-
-    kernel = np.ones((3, 3), dtype=np.uint8)
-    local_max = cv2.dilate(normalized_hough, kernel)  # Dilate the accumulator to find local maxima
-    non_max_suppressed = np.where(normalized_hough == local_max, normalized_hough, 0)
-
-    indices = np.argpartition(non_max_suppressed.flatten(), -nLines)[-nLines:]
-    indices = indices[np.argsort(non_max_suppressed.flatten()[indices])[::-1]]
+    # normalized_hough = img_hough / np.max(img_hough)
+    # kernel = np.ones((3, 3), dtype=np.uint8)
+    # local_max = cv2.dilate(normalized_hough, kernel) 
+    # non_max_suppressed = np.where(normalized_hough == local_max, normalized_hough, 0)
+    # indices = np.argpartition(non_max_suppressed.flatten(), -nLines)[-nLines:]
+    # indices = indices[np.argsort(non_max_suppressed.flatten()[indices])[::-1]]
+            
+    indices = np.argpartition(img_hough.flatten(), -nLines)[-nLines:] # Filters top nLines from the indices
+    indices = indices[np.argsort(img_hough.flatten()[indices])[::-1]] # 
 
     rhos, thetas = np.unravel_index(indices, img_hough.shape)
-
-    # Convert the indices into ρ and θ
     return rhos, thetas
