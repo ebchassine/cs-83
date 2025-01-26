4f705c73f9e9ee621a1daff48d3611a7e3ea909e
ebchassine <ebchassine@gmail.com>
2025-01-26 15:03:59 -0500

    cleaning junk + documentation

--- assgn1/python/myImageFilter.py
+++ assgn1/python/myImageFilter.py
@@ -2,19 +2,18 @@ import numpy as np
 from numpy.lib.stride_tricks import as_strided
 
 def myImageFilter(img0, h):
-    # assert h.shape[0] % 2 == 1 and h.shape[1] % 2 == 1, "Kernel dimensions must be odd."
-
-    # Get dimensions of the image and kernel
     img_height, img_width = img0.shape
     kernel_height, kernel_width = h.shape
 
-    # Pad the input image to handle edges
-    pad_height = kernel_height // 2
-    pad_width = kernel_width // 2
+    pad_height, pad_width = kernel_height // 2, kernel_width // 2
     # padded_img = np.pad(img0, ((pad_height, pad_height), (pad_width, pad_width)), mode='edge')
     padded_img = np.pad(img0, pad_width=pad_width, constant_values=0)
 
-    # Create sliding window view of the image using as_strided
+    # Create sliding window view of the image using as_strided - vectorized method of applying convolution
+    # sources
+    # https://jessicastringham.net/2018/01/01/einsum/
+    # https://sangillee.com/2024-07-27-how-to-compute-faster-conv2d-python/
+    # There were some warnings in the np documentation, but I didn't run into any obvious issues 
     output_shape = (img_height, img_width, kernel_height, kernel_width)
     strides = (
         padded_img.strides[0],  # Stride for rows of the image
@@ -24,19 +23,7 @@ def myImageFilter(img0, h):
     )
     sliding_windows = as_strided(padded_img, shape=output_shape, strides=strides)
 
-    # Perform convolution by multiplying kernel with sliding windows and summing
+    # multiply kernel with sliding windows and summing, refer to blog sources 
     img1 = np.einsum('ijkl,kl->ij', sliding_windows, h)
 
     return img1
-
-
-# if __name__ == "__main__":
-#     img = np.array([[1, 2, 3],
-#                     [4, 5, 6],
-#                     [7, 8, 9]], dtype=np.float32)
-
-#     kernel = np.array([[0, 0, 0],
-#                        [0, 2, 0],
-#                        [0, 0, 0]], dtype=np.float32) / 16.0
-
-#     filtered_img = myImageFilter(img, kernel)
