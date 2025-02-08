ec450d8ca1fc53a79f3f151ab98d7c940dc41ff0
ebchassine <ebchassine@gmail.com>
2025-02-08 18:36:56 -0500

    Changed rot test graph range - correct

--- assign2/python/briefRotTest.py
+++ assign2/python/briefRotTest.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 
 #Q3.5
 #Read the image and convert to grayscale, if necessary
-image_path = "../data/cv_cover.jpg"
+image_path = "./data/cv_cover.jpg"
 img = cv2.imread(image_path)
 # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 histo = [] 
 
-for i in range(36):
+for i in range(1,36): # Excluding 0 rotation - 350 degrees included in the graph - Kishmore advised us to
 	theta = i * 10
 	rotated_image =	 scipy.ndimage.rotate(img, theta) 
 	matches, _, _ = matchPics(img, rotated_image)
@@ -20,8 +20,9 @@ for i in range(36):
 
 # Display histogram
 plt.figure(figsize=(8, 5))
-plt.bar([angle for angle in range(0, 360, 10)], histo, width=8, color='b')
+plt.bar([angle for angle in range(0, 350, 10)], histo, width=10)
+plt.title("BRIEF Descriptor Matching Across Rotations")
 plt.xlabel("Rotation Angle (degrees)")
 plt.ylabel("Number of Matches")
-plt.title("BRIEF Descriptor Matching Across Rotations")
+
 plt.show()
